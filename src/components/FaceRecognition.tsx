602eb8f9017e50c3262f44c9b0e73942fd6ec2a0
yeong1225 <ssucsh1@gmail.com>
2023-08-01 13:10:22 +0900

    hmm

--- src/components/FaceRecognition.tsx
+++ src/components/FaceRecognition.tsx
@@ -12,14 +12,22 @@ const FaceRecognition: React.FC = () => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [detectedLabel, setDetectedLabel] = useState<string | null>(null);
-  const [isModalShownForLabel, setIsModalShownForLabel] = useState<boolean>(
-    false
-  );
+  const [isModalShownForLabel, setIsModalShownForLabel] = useState<boolean>(false);
+  const [isFaceNotDetected, setIsFaceNotDetected] = useState<boolean>(true);
 
+  
+
+  //UseEffect(function, deps)
+  //function : 수행하고자 하는 작업
+  //deps : 배열 형태이며, 배열 안에는 검사하고자 하는 특정 값 or 빈 배열
   useEffect(() => {
     const startWebcam = async () => {
       try {
+        // stream: getUserMedia 호출로부터 해결된 MediaStream 객체로, 사용자의 웹캠에서 나오는 미디어 스트림을 나타냅니다.
         const stream = await navigator.mediaDevices.getUserMedia({
+          //navigator.mediaDevices.getUserMedia
+          //웹캠이나 마이크와 같은 미디어 장치에 대한 접근을 요청하는 웹 API 메서드
+          //이 메서드는 사용자의 웹캠에서 나오는 미디어 스트림을 나타내는 MediaStream 객체를 반환
           video: true,
           audio: false,
         });
@@ -33,17 +41,14 @@ const FaceRecognition: React.FC = () => {
 
     const getLabeledFaceDescriptions = async () => {
       const gymLabel = "gym"; // 설정한 체육관이 사용하는 폴더명 받아오기
-      const labels = ["Daegwon", "IU", "Jane", "jo","Yeongseo"]; // 폴더 내에 있는 파일 이름
+      const labels = ["Daegwon", "IU", "Jane", "jo", "Yeongseo"]; // 폴더 내에 있는 파일 이름
 
       return Promise.all(
         labels.map(async (label) => {
           const descriptions = [];
           const imgUrl = `./labels/${gymLabel}/${label}.jpg`;
           const img = await faceapi.fetchImage(imgUrl);
-          const detections = await faceapi
-            .detectSingleFace(img)
-            .withFaceLandmarks()
-            .withFaceDescriptor();
+          const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
           descriptions.push(detections.descriptor);
 
           return new faceapi.LabeledFaceDescriptors(label, descriptions);
@@ -53,23 +58,20 @@ const FaceRecognition: React.FC = () => {
 
     const setupFaceRecognition = async () => {
       const labeledFaceDescriptors = await getLabeledFaceDescriptions();
-      const faceMatcher = new faceapi.FaceMatcher(
-        labeledFaceDescriptors,
-        UNKNOWN_THRESHOLD
-      );
+      const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, UNKNOWN_THRESHOLD);
 
       const video = videoRef.current;
       const canvas = canvasRef.current;
       const displaySize = { width: video!.width, height: video!.height };
       faceapi.matchDimensions(canvas!, displaySize);
 
-      setInterval(async () => {
-        if (!video!.paused && !video!.ended) {
-          const detections = await faceapi
-            .detectAllFaces(video!)
-            .withFaceLandmarks()
-            .withFaceDescriptors();
 
+      //setInterval : 지정된 시간 간격마다 지정된 기능을 반복하고자 할 때 사용
+      setInterval(async () => {
+        if (!video!.paused && !video!.ended && isFaceNotDetected) { //일시정지 되지 않았을 때니까 //얼굴인식이 안됐을때만 실행
+          const detections = await faceapi.detectAllFaces(video!).withFaceLandmarks().withFaceDescriptors();
+          console.log(isFaceNotDetected);
+          setIsFaceNotDetected(false);
           const resizedDetections = faceapi.resizeResults(detections, displaySize);
           canvas!.getContext("2d")!.clearRect(0, 0, canvas!.width, canvas!.height);
 
@@ -84,6 +86,7 @@ const FaceRecognition: React.FC = () => {
               label = UNKNOWN_LABEL;
             }
 
+            //캔버스 그리는 내용
             const context = canvas!.getContext("2d")!;
             context.lineWidth = 2;
             context.strokeStyle = "red";
@@ -105,22 +108,16 @@ const FaceRecognition: React.FC = () => {
 
             context.fillText(label, x, y + textHeight);
             context.strokeRect(box.x, box.y, box.width, box.height);
-
+            //여기까지
             // Update the detected label and open the modal if the label is not unknown
             if (result.distance <= UNKNOWN_THRESHOLD) {
               setDetectedLabel(label.split(" ")[0]); // Extract the label without confidence score
-              setIsModalOpen(true);
+              setIsFaceNotDetected(false);
+              setIsModalOpen(true); 
             }
-
-            //일치도 확인 가능
-            // if (result.distance <= UNKNOWN_THRESHOLD) {
-            //     const detectedPerson = result.toString();
-            //     setDetectedLabel(detectedPerson);
-            //     setIsModalOpen(true);
-            // }
           });
         }
-      }, 100);
+      }, 1000); //아 이제야 1초임 이전에는 0.1초였음 ;;  1000 = 1초 
     };
 
     Promise.all([
@@ -130,14 +127,31 @@ const FaceRecognition: React.FC = () => {
     ]).then(() => startWebcam().then(setupFaceRecognition));
   }, []);
 
+
   const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setIsModalShownForLabel(true); // Mark the modal as shown for the current detected label
+    console.log(isModalOpen);
+    setIsModalOpen(false); //모달창 닫기
+    console.log(isModalOpen);
+    setIsModalShownForLabel(true); //모달창 닫기 (shownLabel === detectedLabel 다고 지정해놓기)
+    setIsFaceNotDetected(true); //얼굴인식 true로 고침.
+    // Store the detected label in localStorage to prevent showing the modal again for the same label
+    localStorage.setItem("shownLabel", detectedLabel ?? "");
   };
 
   useEffect(() => {
-    // When the detectedLabel changes, reset the isModalShownForLabel state
-    setIsModalShownForLabel(false);
+    // Check if the detectedLabel has already been shown in a previous session
+    //제일 처음 창이 켜질 때 shownLable 값과 detectedLabel 값을 다르게 해주기
+    if (detectedLabel == null) {
+        localStorage.setItem("shownLabel", "null");
+      }
+
+    const shownLabel = localStorage.getItem("shownLabel");
+    if (shownLabel === detectedLabel) {
+      setIsModalShownForLabel(true);
+    } else {
+      setIsModalShownForLabel(false);//이때만 모달창이 열릴 수 있음.
+    }
+
   }, [detectedLabel]);
 
   return (
@@ -151,13 +165,8 @@ const FaceRecognition: React.FC = () => {
         muted
         style={{ margin: "0 auto", position: "absolute" }}
       />
-      <canvas
-        ref={canvasRef}
-        style={{ margin: "0 auto", position: "absolute" }}
-      />
-      <h2 style={{ margin: "0 auto", position: "absolute" }}>
-        {`환영합니다, ${detectedLabel}님!`}
-      </h2>
+      <canvas ref={canvasRef} style={{ margin: "0 auto", position: "absolute" }} />
+      <h2 style={{ margin: "0 auto", position: "absolute" }}>{`환영합니다, ${detectedLabel}님!`}</h2>
 
       <ModalComponent
         isOpen={isModalOpen && !isModalShownForLabel} // Show the modal only if it's open and not shown for the current detected label
@@ -169,3 +178,4 @@ const FaceRecognition: React.FC = () => {
 };
 
 export default FaceRecognition;
+
--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -12,11 +12,20 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
   detectedLabel,
   onClose,
 }) => {
+  const modalStyles = {
+    content: {
+      width: "300px", // 모달 창의 너비를 300px로 설정
+      height: "200px", // 모달 창의 높이를 200px로 설정
+      margin: "auto", // 모달을 화면 가운데로 정렬
+    },
+  };
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Detected Label Modal"
+      style={modalStyles} // 스타일 적용
     >
       <h2>{`환영합니다, ${detectedLabel}님!`}</h2>
       <button onClick={onClose}>Close Modal</button>
@@ -25,5 +34,6 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
 };
 
 export default ModalComponent;
+
 
 
